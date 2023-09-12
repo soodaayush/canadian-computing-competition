d12a1ef7c64f135adaea989abda032ef1a52aaba
DevBaddy <warbaddy19+github@gmail.com>
2023-09-12 18:45:31 -0300

    Added some new lines to 2023 Problem 4

--- 2023.py
+++ 2023.py
@@ -72,18 +72,23 @@ def trianglane(c, lanes):
             nextLane = lanes[arrIndex + 1]
 
         if arrIndex != 0:
-            nextLane = lanes[arrIndex - 1]
+            lastLane = lanes[arrIndex - 1]
 
         if item.count(1) == 2 and nextLane[0] == 1:
             length += 5
+            continue
 
         if item[0] == 1 and lastLane.count(1) == 2:
             length += 0
+            continue
 
         if item.count(1) == 1:
             length += 3
+            continue
 
     return length
 
 
 print(trianglane(7, [[0, 0], [0, 0], [1, 1], [1, 0], [0, 1], [1, 0], [0, 0]]))
+
+# Problem 5

8726e806a1fea2ddd30b99ed564b57df65638958
DevBaddy <warbaddy19+github@gmail.com>
2023-11-26 16:25:45 -0400

    Finished 2022 problem 5

--- 2022/squarePool.py
+++ 2022/squarePool.py
@@ -1,15 +1,28 @@
+def find_largest_square(matrix):
+    rows, cols = len(matrix), len(matrix[0])
+    dp = [[0] * cols for _ in range(rows)]
+    max_size = 0
 
-def squarePool(n, t, trees):
-    w, h = (n, n)
-    arr = [["0" for x in range(w)] for y in range(h)]
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 0:  # Unoccupied element
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
 
-    print(trees[0][0])
+                max_size = max(max_size, dp[i][j])
 
-    for index, i in enumerate(trees):
-        arr[trees[index][0]][trees[index][1]] = "1"
+    return max_size
 
-    for index, i in enumerate(arr):
-        print(arr[index])
+# Example usage
+matrix = [
+    [1, 0, 1, 0, 0],
+    [1, 1, 0, 0, 1],
+    [0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0]
+]
 
-
-print(squarePool(5, 3, [[2, 4],[1,3],[2, 0]]))
+result = find_largest_square(matrix)
+print("Largest square block size:", result)

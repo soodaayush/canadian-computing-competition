4618ac4639914e6e1a0312de47118f3e9b46743e
DevBaddy <warbaddy19+github@gmail.com>
2023-12-16 19:56:36 -0400

    Working on 2021 Problem 4

--- 2021/arrangingBooks.py
+++ 2021/arrangingBooks.py
@@ -9,37 +9,19 @@ sortedBooks.sort()
 if sortedBooks == unsortedBooks:
     print(swaps)
 else:
-    for index, element in enumerate(unsortedBooks):
-        if index == 0:
-            if unsortedBooks[index] == "S" and unsortedBooks[index + 1] == "L" or unsortedBooks[index + 1] == "M":
-                unsortedBooks[index], unsortedBooks[index + 1] = unsortedBooks[index + 1], unsortedBooks[index]
-                swaps = swaps + 1
-            elif unsortedBooks[index] == "M" and unsortedBooks[index + 1] == "L" or unsortedBooks[index + 1] == "S":
-                unsortedBooks[index], unsortedBooks[index + 1] = unsortedBooks[index + 1], unsortedBooks[index]
-                swaps = swaps + 1
-        if index == len(unsortedBooks) - 1:
-            if unsortedBooks[index] == "L" and unsortedBooks[index - 1] == "M":
-                unsortedBooks[index - 1], unsortedBooks[index] = unsortedBooks[index], unsortedBooks[index - 1]
-                swaps = swaps + 1
-            if unsortedBooks[index] == "L" and unsortedBooks[index - 1] == "S":
-                unsortedBooks[index - 1], unsortedBooks[index] = unsortedBooks[index], unsortedBooks[index - 1]
-                swaps = swaps + 1
-            if unsortedBooks[index] == "M" and unsortedBooks[index - 1] == "S":
-                unsortedBooks[index - 1], unsortedBooks[index] = unsortedBooks[index], unsortedBooks[index - 1]
-                swaps = swaps + 1
-            break
-        if unsortedBooks[index] == "S" and unsortedBooks[index + 1] == "L":
-            unsortedBooks[index], unsortedBooks[index + 1] = unsortedBooks[index + 1], unsortedBooks[index]
-            swaps = swaps + 1
-        elif unsortedBooks[index] == "S" and unsortedBooks[index + 1] == "M":
-            unsortedBooks[index], unsortedBooks[index + 1] = unsortedBooks[index + 1], unsortedBooks[index]
-            swaps = swaps + 1
-        elif unsortedBooks[index] == "M" and unsortedBooks[index + 1] == "L":
-            unsortedBooks[index], unsortedBooks[index + 1] = unsortedBooks[index + 1], unsortedBooks[index]
-            swaps = swaps + 1
-        elif unsortedBooks[index] == "M" and unsortedBooks[index - 1] == "S":
-            unsortedBooks[index - 1], unsortedBooks[index] = unsortedBooks[index], unsortedBooks[index - 1]
-            swaps = swaps + 1
+    for i, element in enumerate(unsortedBooks):
+        for j, element2 in enumerate(unsortedBooks):
+            if unsortedBooks[j] == "S" and unsortedBooks[j + 1] == "M":
+                temp = unsortedBooks[j]
+                unsortedBooks[j] = unsortedBooks[j + 1]
+                unsortedBooks[j + 1] = temp
+            if unsortedBooks[j] == "S" and unsortedBooks[j + 1] == "L":
+                temp = unsortedBooks[j]
+                unsortedBooks[j] = unsortedBooks[j + 1]
+                unsortedBooks[j + 1] = temp
+            if unsortedBooks[j] == "M" and unsortedBooks[j + 1] == "L":
+                temp = unsortedBooks[j]
+                unsortedBooks[j] = unsortedBooks[j + 1]
+                unsortedBooks[j + 1] = temp
 
-    if unsortedBooks == sortedBooks:
-        print(swaps)
+print(unsortedBooks)

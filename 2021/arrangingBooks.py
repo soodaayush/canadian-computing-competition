0f9750dcafd16d28019ce420e54d60d039035d82
DevBaddy <warbaddy19+github@gmail.com>
2023-12-17 18:12:04 -0400

    Progress on 2021 Problem 4

--- 2021/arrangingBooks.py
+++ 2021/arrangingBooks.py
@@ -1,24 +1,31 @@
-unsortedBooks = "LLSLMSLSM"
+# unsortedBooks = "LLSLM"
+unsortedBooks = str(input())
+unsortedBooksList = list(unsortedBooks)
+
 unsortedBooks = unsortedBooks.replace("L", "3")
 unsortedBooks = unsortedBooks.replace("M", "2")
 unsortedBooks = unsortedBooks.replace("S", "1")
+
 unsortedBooks = list(unsortedBooks)
-sortedBooks = list(unsortedBooks)
+
+sortedBooks = list(unsortedBooksList)
+sortedBooks.sort()
 
 swaps = 0
+length = len(unsortedBooks) - 1
 
-sortedBooks.sort(reverse=True)
-
-if sortedBooks == unsortedBooks:
-    print(swaps)
+if sortedBooks == unsortedBooksList:
+    print(0)
 else:
-    for i in range(0, len(unsortedBooks) - 1):
-        for j in range(0, len(unsortedBooks) - 1):
-            if unsortedBooks[j] < unsortedBooks[j + 1]:
-                temp = unsortedBooks[j]
-                unsortedBooks[j] = unsortedBooks[j + 1]
-                unsortedBooks[j + 1] = temp
-                swaps = swaps + 1
-    if sortedBooks == unsortedBooks:
-        print(swaps)
+    for j in range(0, length):
+        if unsortedBooks[j] == "3" and unsortedBooks[j - 1] != "3":
+            unsortedBooks.pop(j)
+            unsortedBooks.insert(0, "3")
+            swaps = swaps + 1
+        elif unsortedBooks[j] == "1" and unsortedBooks[j + 1] != "1":
+            unsortedBooks.pop(j)
+            unsortedBooks.append("1")
+            swaps = swaps + 1
+
+    print(swaps)
 

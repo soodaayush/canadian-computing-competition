90adda8d8b598b89502c4d4069c178f34e84bddb
DevBaddy <warbaddy19+github@gmail.com>
2024-01-01 20:06:13 -0400

    Working on 2019 Problem 5

---
+++ 2019/ruleOfThree.py
@@ -0,0 +1,13 @@
+firstRule = str(input())
+secondRule = str(input())
+thirdRule = str(input())
+letters = str(input())
+
+steps = list(letters)[0]
+startingPoint = list(letters)[1]
+endPoint = list(letters)[2]
+
+stepCount = 0
+
+while stepCount <= int(steps):
+    print()

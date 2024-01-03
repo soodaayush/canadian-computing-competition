82c80355264fc715609607b0ec071234f9ef9c6f
DevBaddy <warbaddy19+github@gmail.com>
2024-01-02 20:46:09 -0400

    Still working on problem 5

--- 2019/ruleOfThree.py
+++ 2019/ruleOfThree.py
@@ -1,13 +1,27 @@
-firstRule = str(input())
-secondRule = str(input())
-thirdRule = str(input())
-letters = str(input())
+# firstRule = str(input()).split(" ")
+firstRule = "AA AB".split(" ")
+# secondRule = str(input()).split(" ")
+secondRule = "AB BB".split(" ")
+# thirdRule = str(input()).split(" ")
+thirdRule = "B AA".split(" ")
+# letters = str(input()).split(" ")
+letters = "4 AB AAAB".split(" ")
 
-steps = list(letters)[0]
-startingPoint = list(letters)[1]
-endPoint = list(letters)[2]
+steps = letters[0]
+startingPoint = letters[1]
+endPoint = letters[2]
 
 stepCount = 0
 
-while stepCount <= int(steps):
-    print()
+for i in range(int(steps)):
+    if firstRule[0] in startingPoint:
+        startingPoint = startingPoint.replace(firstRule[0], firstRule[1], 1)
+        continue
+    elif secondRule[0] in startingPoint:
+        startingPoint = startingPoint.replace(secondRule[0], secondRule[1], 1)
+        continue
+    if thirdRule[0] in startingPoint:
+        startingPoint = startingPoint.replace(thirdRule[0], thirdRule[1], 1)
+        continue
+
+print(startingPoint)

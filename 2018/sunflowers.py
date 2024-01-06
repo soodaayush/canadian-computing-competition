29ce68c0e06690278ecc25cd99c04abd92f29590
DevBaddy <warbaddy19+github@gmail.com>
2024-01-06 19:59:40 -0400

    Minor changes

--- 2018/sunflowers.py
+++ 2018/sunflowers.py
@@ -5,7 +5,14 @@ sunflowerArr = []
 for i in range(sunflowerCount):
     sunflowerArr.append(input().split(" "))
 
-smallestNum = min(int(sunflowerArr[0][0]), int(sunflowerArr[0][sunflowerCount - 1]), int(sunflowerArr[sunflowerCount - 1][0]), int(sunflowerArr[sunflowerCount - 1][sunflowerCount - 1]))
+smallestNum = min(int(sunflowerArr[0][0]), int(sunflowerArr[0][sunflowerCount - 1]),
+                  int(sunflowerArr[sunflowerCount - 1][0]), int(sunflowerArr[sunflowerCount - 1][sunflowerCount - 1]))
+
+# var1 = (reversed(sunflowerArr))  # Reverses list
+# print(var1)  # Only prints reverseiterator object, need to convert it to list to see results
+# var2 = list(zip(*var1))  # Groups all elements from the same position into tuples e.g: [[1, 2, 3], [4, 5, 6], [7, 8, 9
+# # ]] -> [(1, 4, 7) (2, 5, 8), (3, 6, 9)]
+# print(var2)
 
 for i in range(10):
     if int(sunflowerArr[0][0]) != smallestNum:

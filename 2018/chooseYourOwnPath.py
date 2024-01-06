502112eadfb3779d8e8513495c4374f241383a23
DevBaddy <warbaddy19+github@gmail.com>
2024-01-06 18:09:03 -0400

    Solution for 2018 problem 4

---
+++ 2018/chooseYourOwnPath.py
@@ -0,0 +1,8 @@
+pages = int(input())
+
+options = []
+
+for i in range(pages):
+    options.append(input().split(" "))
+
+print(options)
--- 2018/sunflowers.py
+++ 2018/sunflowers.py
@@ -5,16 +5,16 @@ sunflowerArr = []
 for i in range(sunflowerCount):
     sunflowerArr.append(input().split(" "))
 
+smallestNum = min(int(sunflowerArr[0][0]), int(sunflowerArr[0][sunflowerCount - 1]), int(sunflowerArr[sunflowerCount - 1][0]), int(sunflowerArr[sunflowerCount - 1][sunflowerCount - 1]))
 
-def rotate90():
-    for j in range(sunflowerCount):
-        for i in range(sunflowerCount - 1, -1, -1):
-            print(sunflowerArr[i][j], end=" ")
-        print()
+for i in range(10):
+    if int(sunflowerArr[0][0]) != smallestNum:
+        var1 = (reversed(sunflowerArr))
+        sunflowerArr = list(zip(*var1))
+        continue
+    if int(sunflowerArr[0][0]) == smallestNum:
+        break
 
 
 for i in sunflowerArr:
-    if sorted(i) != i:
-        rotate90()
-
-print(sunflowerArr)
+    print(*i)

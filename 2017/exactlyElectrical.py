42166dd7adbfd688786d6d4204c7e0e957e5b8c4
DevBaddy <warbaddy19+github@gmail.com>
2024-01-09 20:40:56 -0400

    Completed 2017 problem 3, working on 4

--- 2017/exactlyElectrical.py
+++ 2017/exactlyElectrical.py
@@ -1,1 +1,10 @@
-print("")
+start = input().split(" ")
+destination = input().split(" ")
+steps = int(input())
+
+distance = abs(int(start[0]) - int(destination[0])) + abs(int(start[1]) - int(destination[1]))
+
+if distance <= steps and (steps - distance) % 2 == 0:
+    print('Y')
+else:
+    print('N')
--- 2017/favoriteTimes.py
+++ 2017/favoriteTimes.py
@@ -4,35 +4,19 @@ inputMinutes = int(input())
 
 favoriteTimes = []
 
-# hours = 12
-# minutes = 0
-# totalTimeDistance = 0
-#
 hoursSince12 = 0
 minutesSince12 = 0
-#
-# if inputMinutes >= 60:
-#     hoursSince12 = math.floor(inputMinutes / 60)
-#     minutesSince12 = inputMinutes % 60
-# else:
-#     hoursSince12 = 12
-#     minutesSince12 = inputMinutes
-#
-# if minutesSince12 == 0:
-#     totalTimeDistance = str(hoursSince12) + "00"
-# elif minutesSince12 < 10:
-#     totalTimeDistance = str(hoursSince12) + "0" + str(minutesSince12)
-# elif minutesSince12 >= 10:
-#     totalTimeDistance = str(hoursSince12) + str(minutesSince12)
-#
-# print(totalTimeDistance)
-#
-# # ranges = list(range(1200, int(totalTimeDistance) + 1))
-
-# print(ranges)
 
 if inputMinutes >= 60:
     hoursSince12 = math.floor(inputMinutes / 60)
+
+    if hoursSince12 > 12:
+        for i in range(hoursSince12):
+            if hoursSince12 > 12:
+                hoursSince12 = hoursSince12 % 12
+            else:
+                break
+
     minutesSince12 = inputMinutes % 60
 else:
     hoursSince12 = 12
@@ -50,17 +34,5 @@ elif minutesSince12 >= 10:
 
 print(totalTimeDistance)
 
-# for i in range(1000):
-#     if hours == 12 and minutes >= 34:
-#         favoriteTimes.append("12:34")
-#         hours = 1
-#     if hours == 1:
-#         favoriteTimes.append("1:11")
-#         favoriteTimes.append("1:23")
-#         favoriteTimes.append("1:35")
-#         favoriteTimes.append("1:47")
-#         favoriteTimes.append("1:59")
-#     if hours == 2:
-#         favoriteTimes.append()
 
 

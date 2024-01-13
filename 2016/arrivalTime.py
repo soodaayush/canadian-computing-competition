651853e8dabfe71038896db5679c3cece2f34915
DevBaddy <warbaddy19+github@gmail.com>
2024-01-13 10:02:38 -0400

    Updated problem 4 2016t

--- 2016/arrivalTime.py
+++ 2016/arrivalTime.py
@@ -1,8 +1,24 @@
-import datetime
+from datetime import datetime
+# 05:00
 
-startTime = str(input())
-timeList = list(startTime)
+# 7 - 10 AM RUSH HOUR
+# 3 - 07 PM RUSH HOUR 15 - 19
 
-print(timeList)
+# NORMAL TIME    - 2 HOURS
+# RUSH HOUR TIME - 4 HOURS
 
-if 
+
+startTime = "05:00" # str(input())
+s_time = startTime.split(":")
+hr = s_time[0]
+min = s_time[1]
+normal_hours = 2
+
+if ((int(hr) + normal_hours >= 7) and int(min) > 0) or int(hr) + int(min) + normal_hours <= 10:
+    print('in rush hour')
+else:
+    print('not rush hour')
+
+
+print(hr)
+print(min)

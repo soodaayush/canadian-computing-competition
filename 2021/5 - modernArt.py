f4dbbd908d679c3be74fb9d9a1a8584c653fb151
DevBaddy <warbaddy19+github@gmail.com>
2024-02-10 11:54:33 -0400

    Improved 2021 Problem 5

--- 2021/5 - modernArt.py
+++ 2021/5 - modernArt.py
@@ -2,14 +2,32 @@ M = int(input())
 N = int(input())
 K = int(input())
 
-choices = []
 canvasRows, canvasCols = N, M
+gCount = 0
 
 canvas = [["B"] * canvasRows for _ in range(canvasCols)]
-print(canvas)
 
 for i in range(K):
-    choice = str(input())
-    choices.append(choice)
+    choice = input().split(" ")
 
-print(M, N, choices)
+    if choice[0] == "R":
+        for index, el in enumerate(canvas[int(choice[1]) - 1]):
+            if canvas[int(choice[1]) - 1][index] == "B":
+                canvas[int(choice[1]) - 1][index] = "G"
+                gCount += 1
+            elif canvas[int(choice[1]) - 1][index] == "G":
+                canvas[int(choice[1]) - 1][index] = "B"
+                gCount -= 1
+    if choice[0] == "C":
+        for index, el in enumerate(canvas):
+            if canvas[index][int(choice[1]) - 1] == "B":
+                canvas[index][int(choice[1]) - 1] = "G"
+                gCount += 1
+                continue
+            if canvas[index][int(choice[1]) - 1] == "G":
+                canvas[index][int(choice[1]) - 1] = "B"
+                gCount -= 1
+                continue
+
+print(gCount)
+

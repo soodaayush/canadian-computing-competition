61145a3fd4f83a10cfd84da6a421870a41d514e5
soodaayush <aayush.sood@icloud.com>
2024-12-22 11:49:40 -0400

    Added 2020

---
+++ c++/Senior/2020/surmisingASprintersSpeed.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    double speed = 0;
+    pair<double, double> observation[100005];
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> observation[i].first>>observation[i].second;
+    }
+    sort(observation, observation + n);
+    for (int i = 1; i < n; i++) {
+        speed = max(speed, abs(observation[i].second - observation[i - 1].second)/(observation[i].first - observation[i - 1].first));
+    }
+
+    cout << speed;
+}

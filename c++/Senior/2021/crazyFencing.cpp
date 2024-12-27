934b5e48cbfe69bfcb921cb21efa08e774c304fd
soodaayush <aayush.sood@icloud.com>
2024-12-27 15:42:07 -0400

    Added 2021 S1

---
+++ c++/Senior/2021/crazyFencing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    int total = 0;
+
+    cin >> n;
+
+    vector<int> h(n + 1), w(n);
+
+    for (int i = 0; i <= n; i++) cin >> h[i];
+    for (int i = 0; i < n; i++) cin >> w[i];
+    for (int i = 0; i < n; i++) total += (h[i] + h[i + 1]) * w[i];
+
+    if (total / 2 * 2 != total) cout << total / 2 << ".5" << endl;
+    else cout << total / 2 << endl;
+}

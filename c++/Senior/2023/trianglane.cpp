8d873b4d7674c4478dd0c7c1214f8e3a775800a0
soodaayush <aayush.sood@icloud.com>
2024-12-31 10:11:59 -0400

    Added full 2023 S1 solution

--- c++/Senior/2023/trianglane.cpp
+++ c++/Senior/2023/trianglane.cpp
@@ -3,41 +3,34 @@
 using namespace std;
 
 int main() {
-    int columns = 7;
-//    int columns;
-//    cin >> columns;
-
-    vector<int> row1 {0, 0, 1, 1, 0, 1, 0};
-    vector<int> row2 {0, 0, 1, 0, 1, 0, 0};
-
-//    for (int i = 0; i < columns; i++) cin >> row1[i];
-//    for (int i = 0; i < columns; i++) cin >> row2[i];
-
+    int columns;
     int perimeter = 0;
+    cin >> columns;
+
+    vector<int> row1(columns);
+    vector<int> row2(columns);
+
+    for (int i = 0; i < columns; i++) cin >> row1[i];
+    for (int i = 0; i < columns; i++) cin >> row2[i];
 
     for (int i = 0; i < columns; i++) {
-        if (row1[i] == 1) {
-            perimeter += 3;
+        if (row1[i]) perimeter += 3;
+        if (row2[i]) perimeter += 3;
 
-            if (i > 0 && row1[i - 1] == 1) {
-                perimeter--;
-            }
-
-            if (row2[i] == 1) {
-                perimeter--;
+        if (i % 2 == 0) {
+            if (row1[i] && row2[i]) {
+                perimeter -= 2;
             }
         }
 
-        if (row2[i] == 1) {
-            perimeter += 3;
+        if (i > 0) {
+            if (row1[i] && row1[i - 1]) perimeter -= 1;
+            if (row2[i] && row2[i - 1]) perimeter -= 1;
+        }
 
-            if (i > 0 && row2[i - 1] == 1) {
-                perimeter--;
-            }
-
-            if (row1[i] == 1) {
-                perimeter--;
-            }
+        if (i < columns - 1) {
+            if (row1[i] && row1[i + 1]) perimeter -= 1;
+            if (row2[i] && row2[i + 1]) perimeter -= 1;
         }
     }
 
